a6fc56df70569cd9fe974bdfddadb9a7ca57e045
hoangGiang303 <gianghd303@gmail.com>
2025-03-29 13:30:04 +0700

    .

--- obj/Debug/net9.0/Offset.AssemblyInfo.cs
+++ obj/Debug/net9.0/Offset.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Offset")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd69a3b16fc4f1cfeec9348cbd7e27475f3954a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8acd8cfb1650f24301b8f2d0d7cb50589b039060")]
 [assembly: System.Reflection.AssemblyProductAttribute("Offset")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Offset")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
